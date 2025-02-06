421d1a59f2e7efe2e54d2e618dfd41d6c4d356a1
david_smith <david_smith@sweetwater.com>
2025-02-06 11:44:55 -0500

    main Updated `src/SpapiLwa.php`.

--- src/SpapiLwa.php
+++ src/SpapiLwa.php
@@ -214,7 +214,6 @@ class SpapiLwa
                 CURLOPT_MAXREDIRS => 10,
                 CURLOPT_TIMEOUT => 30,
                 CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
                 CURLOPT_HTTPHEADER => [
                     'accept: application/json',
                     'x-amz-date: '.gmdate('Ymd\THis\Z'),
