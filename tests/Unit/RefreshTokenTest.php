305efff275dee1b3eb506c1e7cb56a03b13914b2
david_smith <david_smith@sweetwater.com>
2025-02-13 15:18:57 -0500

    feat/v2 Updated `tests/Unit/RefreshTokenTest.php`.

--- tests/Unit/RefreshTokenTest.php
+++ tests/Unit/RefreshTokenTest.php
@@ -4,23 +4,22 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Zerotoprod\SpapiLwa\SpapiLwa;
+use Zerotoprod\SpapiLwa\Support\Testing\SpapiLwaResponseFactory;
 
 class RefreshTokenTest extends TestCase
 {
     /** @test */
     public function refresh_token(): void
     {
-        $response = SpapiLwa::from(
-            'client_id',
-            'client_secret',
-            'https://httpbin.org/post',
-            'user-agent'
-        )->refreshToken('refresh_token');
+        SpapiLwa::fake(SpapiLwaResponseFactory::refreshTokenOk());
+
+        $response = SpapiLwa::from('client_id', 'client_secret')
+            ->refreshToken('refresh_token');
 
         self::assertEquals(200, $response['info']['http_code']);
-        self::assertEquals('client_id', $response['response']['form']['client_id']);
-        self::assertEquals('client_secret', $response['response']['form']['client_secret']);
-        self::assertEquals('refresh_token', $response['response']['form']['grant_type']);
-        self::assertEquals('user-agent', $response['response']['form']['user-agent']);
+        self::assertEquals('access_token', $response['response']['access_token']);
+        self::assertEquals('refresh_token', $response['response']['refresh_token']);
+        self::assertEquals('bearer', $response['response']['token_type']);
+        self::assertEquals(3600, $response['response']['expires_in']);
     }
 }
