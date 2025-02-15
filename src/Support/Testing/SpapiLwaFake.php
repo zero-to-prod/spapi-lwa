7a307a9e048dd7d09fe419958ac372121bf6643b
David Smith <dasmith@zero-to-prod.com>
2025-02-15 12:00:56 -0500

    docs/update-docblocks Updated `src/Support/Testing/SpapiLwaFake.php`.

--- src/Support/Testing/SpapiLwaFake.php
+++ src/Support/Testing/SpapiLwaFake.php
@@ -5,6 +5,11 @@ namespace Zerotoprod\SpapiLwa\Support\Testing;
 use Zerotoprod\Container\Container;
 use Zerotoprod\SpapiLwa\Contracts\SpapiLwaInterface;
 
+/**
+ * Fake responses with this class.
+ *
+ * @link https://github.com/zero-to-prod/spapi-lwa
+ */
 class SpapiLwaFake implements SpapiLwaInterface
 {
     /**
@@ -12,23 +17,34 @@ class SpapiLwaFake implements SpapiLwaInterface
      */
     private $response;
 
+    /** @link https://github.com/zero-to-prod/spapi-lwa */
     public function __construct(array $response = [])
     {
         $this->response = $response;
     }
 
+    /**
+     * @inheritDoc
+     * @link https://github.com/zero-to-prod/spapi-lwa
+     */
     public function refreshToken(string $refresh_token, array $options = []): array
     {
         return $this->response;
     }
 
+    /**
+     * @inheritDoc
+     * @link https://github.com/zero-to-prod/spapi-lwa
+     */
     public function clientCredentials(string $scope, array $options = []): array
     {
         return $this->response;
     }
 
     /**
-     * Fakes a response
+     * Fakes a response.
+     *
+     * @link https://github.com/zero-to-prod/spapi-lwa
      */
     public static function fake(array $response = [], ?SpapiLwaInterface $fake = null): SpapiLwaInterface
     {
