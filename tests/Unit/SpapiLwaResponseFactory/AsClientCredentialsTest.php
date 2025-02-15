5581e07eee5aefcd36cd2bdff88da8f5876051cc
David Smith <dasmith@zero-to-prod.com>
2025-02-15 11:24:52 -0500

    v4 Renamed `tests/Unit/ClientCredentialsTest.php` to `tests/Unit/SpapiLwaResponseFactory/AsClientCredentialsTest.php`.

--- tests/Unit/SpapiLwaResponseFactory/AsClientCredentialsTest.php
+++ tests/Unit/SpapiLwaResponseFactory/AsClientCredentialsTest.php
@@ -1,24 +1,29 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Unit\SpapiLwaResponseFactory;
 
 use Tests\TestCase;
 use Zerotoprod\SpapiLwa\SpapiLwa;
+use Zerotoprod\SpapiLwa\Support\Testing\SpapiLwaFake;
 use Zerotoprod\SpapiLwa\Support\Testing\SpapiLwaResponseFactory;
 
-class ClientCredentialsTest extends TestCase
+class AsClientCredentialsTest extends TestCase
 {
     /** @test */
-    public function client_credentials(): void
+    public function asClientCredentials(): void
     {
-        SpapiLwa::fake(SpapiLwaResponseFactory::clientCredentialsOk());
+        SpapiLwaFake::fake(
+            SpapiLwaResponseFactory::factory()
+                ->asClientCredentialsResponse()
+                ->make()
+        );
 
         $response = SpapiLwa::from('client_id', 'client_secret')
             ->clientCredentials('scope');
 
         self::assertEquals(200, $response['info']['http_code']);
         self::assertEquals('access_token', $response['response']['access_token']);
-        self::assertEquals('sellingpartnerapi::notifications', $response['response']['scope']);
+        self::assertEquals('scope', $response['response']['scope']);
         self::assertEquals('bearer', $response['response']['token_type']);
         self::assertEquals(3600, $response['response']['expires_in']);
     }
